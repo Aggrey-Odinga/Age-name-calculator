586b76613afe8e7644c5c103e03080f2f66c6dd8
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-03-05 08:39:44 +0300

    Provide validation functions for false inputs

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function age(){
     var FemaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
     var Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        if (strDay == ""){
+        alert("please enter day")
+        return
+    } 
+    if (strMonth == ""){
+        alert("please enter month")
+        return
+    }
+    if (strYear == ""){
+        alert("please enter year")
+        return
+    }
+
+    var day = parseInt(strDay);
+    if (day < 1 || day > 31){
+        alert("invalid day of the month, please enter valid day")
+        return
+    }
+    var month = parseInt(strMonth);
+    if (month <  1 || month > 12){
+        alert("invalid month, please enter valid month")
+        return
+    }
+
+    if (male.checked == false && female.checked == false){
+        alert("select gender")
+        return
+    } 
