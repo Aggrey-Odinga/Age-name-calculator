54405bc13ab562e63bfc12086c085f63de632991
Aggrey Odinga <aggrey.odinga@student.moringaschool.com>
2022-03-05 08:31:26 +0300

    Provide logic for invoking javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+function age(){
+    console.log("test")
+    var form = document.getElementById('form_age');
+    console.log(form.elements["date"].value)
+
+
+    var strDay = document.getElementById('date').value;
+    var strMonth = document.getElementById('month').value;
+    var strYear = document.getElementById('year').value;
+    var male = document.getElementById('male');
+    var female = document.getElementById('female');
+    console.log(typeof male.checked)
+    console.log(typeof false)
+    
